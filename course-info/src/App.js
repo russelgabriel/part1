0b9c639280fb36bcad40bcf49a9d0c000d0690b5
russelsy <russelgabrielsy@gmail.com>
2022-06-26 16:03:12 -0400

    finished 1.3

--- course-info/src/App.js
+++ course-info/src/App.js
@@ -1,27 +1,58 @@
 const App = () => {
   const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  }
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  }
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>
-        Number of exercises {exercises1 + exercises2 + exercises3}
-      </p>
+      <Header header={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total a={part1} b={part2} c={part3} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  const title = props.header
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1.name} exercises={props.part1.exercises} />
+      <Part part={props.part2.name} exercises={props.part2.exercises} />
+      <Part part={props.part3.name} exercises={props.part3.exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.a.exercises + props.b.exercises + props.c.exercises} </p>
     </div>
   )
 }
--- practice/src/App.js
+++ practice/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const arr = ["fuck", "shit", "bitch"]
 
-function App() {
+  arr.forEach(value => {console.log(value);})
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p></p>
     </div>
-  );
+  )
 }
 
 export default App;
